acf50df53c5f55bd260486cacbb8c62ca25dbb15
Mridhul <mridhul67@gmail.com>
2022-11-17 00:58:38 +0530

    Append the textcontent of numbers to display upon click

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Algorithm
 // Create functions for add, subtract, multiply and divide with two numbers as its parameters, return the result value
 // Create a function operate to call the right operation function: take the operation and two numbers as its parameters
+// Create an event listener which listens to click on number buttons and copies the textcontent of buttons into display
+// Append each number clicked to the textcontent of the display using string property
 
 function add(num1, num2){
     let result = num1 + num2;
@@ -25,3 +27,12 @@ function divide(num1, num2){
 function operate(operation, num1, num2){
     return operation(num1,num2);
 }
+
+const display = document.querySelector('.display');
+const numbers = document.querySelectorAll('.numberBtn');
+
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+        display.textContent += number.textContent;
+    })
+});
